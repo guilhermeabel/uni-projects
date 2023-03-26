d788ec8b3385fc861366c45733adb17d73fff87b
Guilherme Abel <guilhermeabel8@gmail.com>
2023-03-26 12:15:15 -0300

    add: ListaDeLivros

---
+++ integrador-3/src/ListaDeLivros.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ListaDeLivros {
+    private List<Livro> livros;
+
+    public ListaDeLivros() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void incluirNoInicio(Livro livro) {
+        this.livros.add(0, livro);
+    }
+
+    public void incluirNoFim(Livro livro) {
+        this.livros.add(livro);
+    }
+
+    public void ordenar() {
+        this.livros.sort(Comparator.comparing(Livro::getTitulo));
+    }
+
+    public Livro removerDoFim() {
+        if (!this.livros.isEmpty()) {
+            Livro livroRemovido = this.livros.get(this.livros.size() - 1);
+            this.livros.remove(this.livros.size() - 1);
+            return livroRemovido;
+        } else {
+            return null;
+        }
+    }
+
+    public int tamanho() {
+        return this.livros.size();
+    }
+
+    public Livro get(int index) {
+        if (index >= 0 && index < this.livros.size()) {
+            return this.livros.get(index);
+        } else {
+            return null;
+        }
+    }
+}
