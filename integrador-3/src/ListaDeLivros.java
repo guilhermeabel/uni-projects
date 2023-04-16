c4f41c74b8a50fb8e55ba2152f03de9fb7fff007
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-16 17:00:49 -0300

    add: lista de livros throws exception

--- integrador-3/src/ListaDeLivros.java
+++ integrador-3/src/ListaDeLivros.java
@@ -2,44 +2,51 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import Exceptions.ExcecaoDeLivroJaExistente;
+
 public class ListaDeLivros {
-    private List<Livro> livros;
+	private List<Livro> livros;
 
-    public ListaDeLivros() {
-        this.livros = new ArrayList<>();
-    }
+	public ListaDeLivros() {
+		this.livros = new ArrayList<>();
+	}
 
-    public void incluirNoInicio(Livro livro) {
-        this.livros.add(0, livro);
-    }
+	public void incluirNoInicio(Livro livro) {
+		this.livros.add(0, livro);
+	}
 
-    public void incluirNoFim(Livro livro) {
-        this.livros.add(livro);
-    }
+	public void incluirNoFim(Livro livro) throws ExcecaoDeLivroJaExistente {
+		for (Livro livroExistente : livros) {
+			if (livroExistente.equals(livro)) {
+				throw new ExcecaoDeLivroJaExistente("O livro já existe na lista.");
+			}
+		}
+		livros.add(livro);
+	}
 
-    public void ordenar() {
-        this.livros.sort(Comparator.comparing(Livro::getTitulo));
-    }
+	public void ordenar() {
+		this.livros.sort(Comparator.comparing(Livro::getTitulo));
+	}
 
-    public Livro removerDoFim() {
-        if (!this.livros.isEmpty()) {
-            Livro livroRemovido = this.livros.get(this.livros.size() - 1);
-            this.livros.remove(this.livros.size() - 1);
-            return livroRemovido;
-        } else {
-            return null;
-        }
-    }
+	public Livro removerDoFim() {
+		if (!this.livros.isEmpty()) {
+			Livro livroRemovido = this.livros.get(this.livros.size() - 1);
+			this.livros.remove(this.livros.size() - 1);
+			return livroRemovido;
+		} else {
+			return null;
+		}
+	}
 
-    public int tamanho() {
-        return this.livros.size();
-    }
+	public int tamanho() {
+		return this.livros.size();
+	}
 
-    public Livro get(int index) {
-        if (index >= 0 && index < this.livros.size()) {
-            return this.livros.get(index);
-        } else {
-            return null;
-        }
-    }
+	public Livro get(int index) {
+		if (index >= 0 && index < this.livros.size()) {
+			return this.livros.get(index);
+		} else {
+			return null;
+		}
+	}
 }
