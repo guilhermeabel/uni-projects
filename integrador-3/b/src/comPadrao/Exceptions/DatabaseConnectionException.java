fdc2ccc83140912165243faa56b597f6a1f0f7b5
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-03 20:58:36 -0300

    add: classes para exceptions

---
+++ integrador-3/b/src/comPadrao/Exceptions/DatabaseConnectionException.java
@@ -0,0 +1,7 @@
+package comPadrao.Exceptions;
+
+public class DatabaseConnectionException extends Exception {
+	public DatabaseConnectionException(String message) {
+		super(message);
+	}
+}
---
+++ integrador-3/b/src/comPadrao/Exceptions/JDBCDriverNotFoundException.java
@@ -0,0 +1,7 @@
+package comPadrao.Exceptions;
+
+public class JDBCDriverNotFoundException extends Exception {
+	public JDBCDriverNotFoundException(String message) {
+		super(message);
+	}
+}
