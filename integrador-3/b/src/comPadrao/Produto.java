83e86e0ce8d60464233932851fa768bcb2b9f179
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-03 21:00:39 -0300

    add: entidade de produto

---
+++ integrador-3/b/src/comPadrao/Produto.java
@@ -0,0 +1,37 @@
+package comPadrao;
+
+public class Produto {
+	private int codigo;
+	private String nome;
+	private String descricao;
+
+	public Produto(int codigo, String nome, String descricao) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
